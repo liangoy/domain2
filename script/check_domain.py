8ca680b369b2e0f19c7cc059a264d354960ccbe2
liangoy <emailboxbeta@gmail.com>
2018-03-12 11:21:52 +0800

    ?

--- script/check_domain.py
+++ script/check_domain.py
@@ -4,14 +4,14 @@ from utils.agent import is_registered
 if __name__ == '__main__':
     while True:
         try:
-            domains = [i['domain'] for i in DB.digit6.find({'status': ''}).limit(64)]
+            domains = [i['domain'] for i in DB.digit6.find({'status': {'$in': ['', -1]}}).limit(64)]
             output = is_registered(domains)
             for i in output:
                 DB.digit6.update_one({'domain': i['domain']}, {'$set': {'status': i['status']}})
 
-            a1=len([i for i in output if i['status']==1])
-            a_1=len([i for i in output if i['status']==-1])
-            a0=len([i for i in output if i['status']==0])
-            print(a0,a1,a_1)
+            a1 = len([i for i in output if i['status'] == 1])
+            a_1 = len([i for i in output if i['status'] == -1])
+            a0 = len([i for i in output if i['status'] == 0])
+            print(a0, a1, a_1)
         except Exception as e:
             print(e)
